09979eaac7c06f9b7eff197adefa06ec0ca4fc40
DarK LorD <lordvictor6393@gmail.com>
2017-06-15 23:06:04 -0400

    Adding bootstrap config, components structure, and header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Victor on 15/06/2017.
+ */
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+}
