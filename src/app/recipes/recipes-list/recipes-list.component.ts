486f45544290b3dfd0ada37a93b84829cac6350a
VictorVasquez <victor.vasquez.nrd@gmail.com>
2018-03-19 18:55:51 -0400

    Moving recipe list item to its respective component

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -10,7 +10,8 @@ import { Recipe } from '../recipe.model';
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('test recipe', 'test recipe description', 'http://www.potato.ie/media/1008/cheesy-potato-bites.jpg'),
-    new Recipe('test recipe', 'test recipe description', 'http://www.potato.ie/media/1008/cheesy-potato-bites.jpg')
+    new Recipe('test recipe2', 'test recipe description2', 'http://www.potato.ie/media/1008/cheesy-potato-bites.jpg'),
+    new Recipe('test recipe3', 'test recipe description3', 'http://www.potato.ie/media/1008/cheesy-potato-bites.jpg')
   ];
 
   constructor() { }
