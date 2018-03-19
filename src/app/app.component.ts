07964dc78515a8e941b9eb060c9cac6efe2b78e6
VictorVasquez <victor.vasquez.nrd@gmail.com>
2018-03-19 18:24:39 -0400

    Adding navigation (First Approach)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  activeTab = "recipes";
+  onActiveTabChange(activeTabName: string) {
+    this.activeTab = activeTabName;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,18 @@
 /**
  * Created by Victor on 15/06/2017.
  */
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  @Output() activeTabChanged = new EventEmitter<string>();
 
+  ngOnInit() {
+  }
+
+  onTabSelect(selectedTab: string) {
+    this.activeTabChanged.emit(selectedTab);
+  }
 }
