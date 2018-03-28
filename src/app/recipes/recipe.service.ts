960926abb8f8804e7b7c038b07b113417210fbe2
VictorVasquez <victor.vasquez.nrd@gmail.com>
2018-03-28 12:38:23 -0400

    Adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,38 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     selectedRecipe = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('test recipe', 'test recipe description', 'http://www.potato.ie/media/1008/cheesy-potato-bites.jpg'),
-        new Recipe('test recipe2', 'test recipe description2', 'http://www.potato.ie/media/1008/cheesy-potato-bites.jpg'),
-        new Recipe('test recipe3', 'test recipe description3', 'http://www.potato.ie/media/1008/cheesy-potato-bites.jpg')
+        new Recipe(
+            'cheesy potato', 
+            'cheesy potato description', 
+            'http://www.potato.ie/media/1008/cheesy-potato-bites.jpg',
+            [
+                new Ingredient('cheese', 1),
+                new Ingredient('potato', 5),
+            ]
+        ),
+        new Recipe(
+            'test recipe2', 
+            'test recipe description2', 
+            'http://www.potato.ie/media/1008/cheesy-potato-bites.jpg',
+            [
+                new Ingredient('apple', 3),
+                new Ingredient('banana', 2),
+            ]
+        ),
+        new Recipe(
+            'test recipe3', 
+            'test recipe description3', 
+            'http://www.potato.ie/media/1008/cheesy-potato-bites.jpg',
+            [
+                new Ingredient('bread', 2),
+                new Ingredient('pear', 10)
+            ]
+        )
       ];
 
     getRecipes() {
